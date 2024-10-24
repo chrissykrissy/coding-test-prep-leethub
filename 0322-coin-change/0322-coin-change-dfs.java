213419e1278e354cca1e5072ddaea316b901e28a
chrissykrissy <jj2174@nyu.edu>
2024-10-24 18:43:11 +0900

    Time: 35 ms (16.62%), Space: 45 MB (28.05%) - LeetHub

--- 0322-coin-change/0322-coin-change-dfs.java
+++ 0322-coin-change/0322-coin-change-dfs.java
@@ -1,16 +1,21 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp, amount+1);
-        dp[0] = 0;
-        for(int c = 0; c <= amount; c++){
-            for(int i = 0; i < coins.length; i++){
-                if(coins[i] <= c){
-                    dp[c] = Math.min(dp[c], dp[c-coins[i]]+1);
-                }
+        if(amount < 1) return 0;
+        return getCount(coins, amount, new int[amount]);
+    }
+
+    public int getCount(int[] coins, int rem, int[] dp){
+        if(rem < 0) return -1;
+        if(rem == 0) return 0;
+        if(dp[rem-1] != 0) return dp[rem-1];
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins){
+            int res = getCount(coins, rem-coin, dp);
+            if (res >= 0 && res < min){
+                min = 1 + res;
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        dp[rem-1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return dp[rem-1];
     }
 }
