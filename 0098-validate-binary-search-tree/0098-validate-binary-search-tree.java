6082a0a69c574bcbb6b320600a8877248cef3b31
chrissykrissy <jj2174@nyu.edu>
2024-10-25 13:45:58 +0900

    Time: 2 ms (15.39%), Space: 42.8 MB (95.58%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,18 +15,19 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidLeaf(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    public boolean isValidLeaf(TreeNode root, long lower, long upper){
-        if(root == null || root.right == null && root.left == null) return true;
-        if(root.left != null && (root.left.val >= root.val || root.left.val >= upper || root.left.val <= lower)){
-            return false;
+        if(root == null) return true;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode pre = null;
+        while(root != null || !stk.isEmpty()){
+            while(root != null){
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
         }
-        if(root.right != null && (root.right.val <= root.val || root.right.val <= lower || root.right.val >= upper)){
-            return false;
-        }
-        boolean right = isValidLeaf(root.right, root.val, upper);
-        boolean left = isValidLeaf(root.left, lower, root.val);
-        return left && right;    
+        return true;
     }
 }
