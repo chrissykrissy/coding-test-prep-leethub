8af74c4954acb9480217c02cd1331b3388e56e47
chrissykrissy <jj2174@nyu.edu>
2024-10-25 16:00:45 +0900

    Time: 2 ms (72.97%), Space: 42.3 MB (50.84%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges-bfsWithCount.java
@@ -0,0 +1,33 @@
+class Solution {
+    int time = 0; 
+    int fresh = 0;
+    int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j, 0});
+                }
+                if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            time = Math.max(time, curr[2]);
+            for(int[] d : dir){
+                int nx = curr[0]+d[0];
+                int ny = curr[1]+d[1];
+                if(nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[nx].length && grid[nx][ny] == 1){
+                    fresh--;
+                    q.offer(new int[]{nx, ny, curr[2]+1});
+                    grid[nx][ny] = 2;
+                }
+            }
+        }
+        
+        return fresh == 0 ? time : -1;
+    }
+}
