c2e4171b171569e4011312718091a7e547763372
chrissykrissy <jj2174@nyu.edu>
2024-10-28 18:24:04 +0900

    Time: 136 ms (77.88%), Space: 42 MB (25.74%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,8 +3,7 @@ class Solution {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[i].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    boolean[][] visited = new boolean[board.length][board[0].length];
-                    boolean exist = dfs(board, i, j, word, 0, visited);
+                    boolean exist = dfs(board, i, j, word, 0);
                     if(exist) return true;
                 }
             }
@@ -12,17 +11,18 @@ class Solution {
         return false;
     }
 
-    private boolean dfs(char[][] board, int i, int j, String s, int index, boolean[][] visited){
+    private boolean dfs(char[][] board, int i, int j, String s, int index){
         if(index == s.length()){
             return true;
         }
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || index > s.length() || board[i][j] != s.charAt(index)){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#' || index > s.length() || board[i][j] != s.charAt(index)){
             return false;
         }
-        visited[i][j] = true;
-        boolean result = dfs(board, i+1, j, s, index+1, visited) || dfs(board, i-1, j, s, index+1, visited)
-        || dfs(board, i, j-1, s, index+1, visited) || dfs(board, i, j+1, s, index+1, visited);
-        visited[i][j] = false;
+        char ori = board[i][j];
+        board[i][j] = '#';
+        boolean result = dfs(board, i+1, j, s, index+1) || dfs(board, i-1, j, s, index+1)
+        || dfs(board, i, j-1, s, index+1) || dfs(board, i, j+1, s, index+1);
+        board[i][j] = ori;
         return result;
     }
 }
