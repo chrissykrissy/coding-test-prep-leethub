d8bad291af1b0bb9feecc83867aff8336c4dd65e
chrissykrissy <jj2174@nyu.edu>
2024-10-31 17:50:23 +0900

    Time: 21 ms (5.85%), Space: 45 MB (14.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,46 +1,15 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] checkRow;
-        int[] checkCol;
-        for(int i = 0; i < board.length; i++){
-            checkRow = new int[10];
-            for(int j = 0; j < board.length; j++){
-                if(board[i][j] != '.'){
-                    if(checkRow[board[i][j]-'0'] > 0){
+        Set seen = new HashSet();
+        for (int i=0; i<9; ++i) {
+            for (int j=0; j<9; ++j) {
+                if (board[i][j] != '.') {
+                    String b = "(" + board[i][j] + ")";
+                    if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
                         return false;
-                    }
-                    checkRow[board[i][j]-'0']++;
                 }
             }
         }
-        for(int i = 0;  i < board.length; i++){
-            checkCol = new int[10];
-            for(int j = 0; j < board.length; j++){
-                if(board[j][i] != '.'){
-                    if(checkCol[board[j][i]-'0'] > 0){
-                        return false;
-                    }
-                    checkCol[board[j][i]-'0']++;
-                }
-            }
-        }
-
-                for (int startRow = 0; startRow < board.length; startRow += 3) {
-            for (int startCol = 0; startCol < board[0].length; startCol += 3) {
-                int[] checkBox = new int[10];
-                for (int row = startRow; row < startRow + 3; row++) {
-                    for (int col = startCol; col < startCol + 3; col++) {
-                        if (board[row][col] != '.') {
-                            if (checkBox[board[row][col] - '0'] > 0) {
-                                return false;
-                            }
-                            checkBox[board[row][col] - '0']++;
-                        }
-                    }
-                }
-            }
-        }
-
         return true;
     }
 }
