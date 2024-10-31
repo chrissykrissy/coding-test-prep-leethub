45ce7285ed5a624be269ffdff35865fc4fbae93b
chrissykrissy <jj2174@nyu.edu>
2024-10-31 15:35:27 +0900

    Time: 0 ms (100%), Space: 43.1 MB (23.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,36 @@
 class Solution {
-        /*
-    Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
-    Find the largest index l greater than k such that a[k] < a[l].
-    Swap the value of a[k] with that of a[l].
-    Reverse the sequence from a[k + 1] up to and including the final element a[n].
-     */
+    /*
+    -Find largest index k, a[k] < a[k+1]
+        - if don't exist, it's the last permu (reverse the whole)
+    - find largest index l greater than k, a[k] < a[l]
+    - swap a[k] w/ a[l]
+    - reverse a[k+1] till a[n]
+    */
     public void nextPermutation(int[] nums) {
-        int pivot = -1;
-        for (int i = 0; i < nums.length-1; i++) {
+        int k = -1;
+        for(int i = 0; i < nums.length-1;i++){
             if(nums[i] < nums[i+1]){
-                pivot = i;
+                k = i;
             }
         }
-        int l = -1;
-        if(pivot != -1){
-            for (int i = 0; i < nums.length; i++) {
-                if(nums[pivot] < nums[i]){
-                    l = Math.max(l, i);
-                }
-            }
-            int temp = nums[pivot];
-            nums[pivot] = nums[l];
-            nums[l] = temp;
-            reverse(nums, pivot+1, nums.length-1);
-        }else{
+        if(k == -1) {
             reverse(nums, 0, nums.length-1);
-
+            return;
         }
+
+        int l = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > nums[k]){
+                l = i;
+            }
+        }
+
+        int temp = nums[k];
+        nums[k] = nums[l];
+        nums[l] = temp;
+        reverse(nums, k+1, nums.length-1);
     }
+
     public void reverse(int[] nums, int i, int j){
         while(i < j){
             int temp = nums[i];
