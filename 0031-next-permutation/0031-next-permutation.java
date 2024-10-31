5a2ff1bdbb0ef955862d348dc037cafbd37b21b1
chrissykrissy <jj2174@nyu.edu>
2024-10-31 15:31:18 +0900

    Time: 0 ms (100%), Space: 43 MB (56.92%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,40 @@
 class Solution {
+        /*
+    Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+    Find the largest index l greater than k such that a[k] < a[l].
+    Swap the value of a[k] with that of a[l].
+    Reverse the sequence from a[k + 1] up to and including the final element a[n].
+     */
     public void nextPermutation(int[] nums) {
-        int k = -1;
-        for(int i = 0; i < nums.length-1;i++){
+        int pivot = -1;
+        for (int i = 0; i < nums.length-1; i++) {
             if(nums[i] < nums[i+1]){
-                k = i;
+                pivot = i;
             }
         }
-        if(k == -1) {
-            Arrays.sort(nums);
-            return;
-        }
-
         int l = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > nums[k]){
-                l = i;
+        if(pivot != -1){
+            for (int i = 0; i < nums.length; i++) {
+                if(nums[pivot] < nums[i]){
+                    l = Math.max(l, i);
+                }
             }
+            int temp = nums[pivot];
+            nums[pivot] = nums[l];
+            nums[l] = temp;
+            reverse(nums, pivot+1, nums.length-1);
+        }else{
+            reverse(nums, 0, nums.length-1);
+
         }
-
-        int temp = nums[k];
-        nums[k] = nums[l];
-        nums[l] = temp;
-        reverse(nums, k+1, nums.length-1);
-        
     }
-
     public void reverse(int[] nums, int i, int j){
         while(i < j){
             int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
