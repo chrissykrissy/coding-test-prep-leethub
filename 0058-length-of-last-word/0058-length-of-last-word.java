336cccd2b886bc0c8a21177d06416332e1335ce4
chrissykrissy <jj2174@nyu.edu>
2024-11-12 00:28:43 +0900

    Time: 0 ms (100%), Space: 41.6 MB (67.35%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.split(" ");
-        return str[str.length-1].length();
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0) return length;
+            }
+        }
+        return length;
     }
 }
