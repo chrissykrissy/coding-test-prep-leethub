3d018f48cfe85264856066edce4075601d5b4437
chrissykrissy <jj2174@nyu.edu>
2024-11-14 00:19:33 +0900

    Time: 1 ms (100%), Space: 58.2 MB (21.57%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -3,16 +3,11 @@ class Solution {
          int sum = 0;
          int count = Integer.MAX_VALUE;
          int i = 0;
-         int j = 0;
-         while (i <= j && j < nums.length) {
+        for (int j = 0; j < nums.length; j++) {
             sum += nums[j];
-            if (sum < target) {
-                j++;
-            } else {
-                count = Math.min(j-i+1, count);
-                i++;
-                j = i;
-                sum = 0;
+            while (sum >= target) {
+                count = Math.min(count, j - i + 1);
+                sum -= nums[i++];
             }
         }
         return count == Integer.MAX_VALUE ? 0 : count;
