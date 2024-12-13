3d7aa96ee57df29b6fb7b1941122eef2ffd9d724
chrissykrissy <chrissy.jeon0716@gmail.com>
2024-12-13 18:43:26 +0900

    Time: 0 ms (100%), Space: 42.3 MB (34.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //two binary searches
-
-        //search row
-        int top = 0, bot = matrix.length-1;
-        while(top <= bot){
-            int mid = top + (bot - top) / 2;
-            if(target > matrix[mid][matrix[0].length-1]){
-                top = mid + 1;
-            }else if (target < matrix[mid][0]){
-                bot = mid - 1;
-            }else{
+        //two binary search
+        //find row
+        int i = 0, j = matrix.length-1;
+        while (i <= j) {
+            int mid = (i + j)/2;
+            //행이 마지막인지 확인하거나, 다음 행의 첫 번째 값보다 target이 작은지를 조건에 추가.
+            if (matrix[mid][0] <= target && (mid == matrix.length - 1 || matrix[mid + 1][0] > target)) {
+                i = mid;
                 break;
             }
+            if (matrix[mid][0] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
         }
-        if(top > bot) return false;
-        int row = top + (bot-top)/2;
-        int l = 0, r = matrix[0].length-1;
-        while(l <= r){
-            int m =  l + (r - l)/2;
-            if (target > matrix[row][m]){
-                l = m + 1;
-            }else if (target < matrix[row][m]){
-                r = m - 1;
-            }else{
+
+        int k = 0;
+        int col = matrix[i].length-1;
+        while (k <= col) {
+            int mid = (k + col)/2;
+            if (matrix[i][mid] == target) {
                 return true;
+            }
+            if (matrix[i][mid] > target) {
+                col = mid - 1;
+            }else {
+                k = mid + 1;
             }
         }
         return false;
