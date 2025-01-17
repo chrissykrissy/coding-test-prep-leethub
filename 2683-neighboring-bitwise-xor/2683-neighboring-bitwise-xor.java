4cfbc126dfecd690c2712c47d2eb30798da93872
chrissykrissy <chrissy.jeon0716@gmail.com>
2025-01-17 20:49:59 +0900

    Time: 2 ms (100%), Space: 62.6 MB (32.94%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int sum = 0;
+        for (int i : derived) {
+            sum ^= i;
+        }
+
+        if (sum == 0) return true;
+        return false;
+    }
+}
