9a259f6ec42a46303a49e1e8adb1cd125d9eab2c
chrissykrissy <chrissy.jeon0716@gmail.com>
2025-02-05 20:05:17 +0900

    Time: 0 ms (100%), Space: 41.7 MB (58.39%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int first = 0;
+        int second = 0;        
+        int count = 0;
+        
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                if (count > 2) {
+                    return false;
+                } else if (count == 1){
+                    first = i;
+                } else {
+                    second = i;
+                }
+            }
+        }
+
+        return s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first);
+    }
+}
