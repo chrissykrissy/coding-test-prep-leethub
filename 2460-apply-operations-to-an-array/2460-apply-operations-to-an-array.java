c04de6df08a2afba2114c850ba90b44617d121ee
chrissykrissy <chrissy.jeon0716@gmail.com>
2025-03-02 00:50:00 +0900

    Time: 1 ms (96.37%), Space: 44 MB (27.8%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i] == nums[i+1]) {
+            if (nums[i] == nums[i+1] && nums[i] != 0) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
         }
 
+        int nonZ = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                int swap = i;
-                while (swap < nums.length) {
-                    if (nums[swap] != 0) {
-                        nums[i] = nums[swap];
-                        nums[swap] = 0;
-                        break;
-                    }
-                    swap++;
-                }
+            if (nums[i] != 0) {
+                nums[nonZ++] = nums[i];
             }
+        }
+
+        while (nonZ < nums.length) {
+            nums[nonZ++] = 0;
         }
         return nums;
     }
