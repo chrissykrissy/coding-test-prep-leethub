a017e845b75d356e5174dc64d84e2770cc5cc576
chrissykrissy <chrissy.jeon0716@gmail.com>
2025-03-03 01:43:10 +0900

    Time: 1 ms (86.98%), Space: 45.2 MB (78.89%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,26 +1,51 @@
 class Solution {
+
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        Map<Integer,Integer> map = new HashMap<>();
+        int N1 = nums1.length, N2 = nums2.length;
+        int ptr1 = 0, ptr2 = 0;
 
-        for (int i = 0; i < nums1.length; i++) {
-            map.put(nums1[i][0], nums1[i][1]);
-        }
-
-        for (int i = 0; i < nums2.length; i++) {
-            if (map.containsKey(nums2[i][0])) {
-                map.put(nums2[i][0], map.get(nums2[i][0]) + nums2[i][1]);
-            }else {
-                map.put(nums2[i][0], nums2[i][1]);
+        List<int[]> mergedArray = new ArrayList<>();
+        while (ptr1 < N1 && ptr2 < N2) {
+            // If the id is same, add the values and insert to the result.
+            // Increment both pointers.
+            if (nums1[ptr1][0] == nums2[ptr2][0]) {
+                mergedArray.add(
+                    new int[] {
+                        nums1[ptr1][0],
+                        nums1[ptr1][1] + nums2[ptr2][1],
+                    }
+                );
+                ptr1++;
+                ptr2++;
+            } else if (nums1[ptr1][0] < nums2[ptr2][0]) {
+                // If the id in nums1 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums1[ptr1]);
+                ptr1++;
+            } else {
+                // If the id in nums2 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums2[ptr2]);
+                ptr2++;
             }
         }
 
-        List<Integer> keys = new ArrayList(map.keySet());
-        Collections.sort(keys);
-        int[][] ret = new int[keys.size()][2];
-        for (int i = 0; i < keys.size(); i++) {
-            ret[i][0] = keys.get(i);
-            ret[i][1] = map.get(ret[i][0]);
+        // If pairs are remaining in the nums1, then add them to the result.
+        while (ptr1 < N1) {
+            mergedArray.add(nums1[ptr1]);
+            ptr1++;
         }
-        return ret;
+
+        // If pairs are remaining in the nums2, then add them to the result.
+        while (ptr2 < N2) {
+            mergedArray.add(nums2[ptr2]);
+            ptr2++;
+        }
+
+        // Convert List<int[]> to int[][]
+        int[][] result = new int[mergedArray.size()][2];
+        for (int i = 0; i < mergedArray.size(); i++) {
+            result[i] = mergedArray.get(i);
+        }
+
+        return result;
     }
 }
